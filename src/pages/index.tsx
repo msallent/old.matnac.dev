172e11dfcf631d058ca29229fdae12232820f0a8
Matias Sallent <mqsallent@gmail.com>
2021-06-24 00:58:48 -0300

    Add animated logo to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 import { PageProps } from 'gatsby';
-import { Intro } from '../components/Intro/Intro';
+import { Defs } from '../components/Defs/Defs';
+import { TracedM } from '../components/TracedM/TracedM';
+import { Logo } from '../components/Logo/Logo';
 
-const Index: FunctionComponent<PageProps> = () => <Intro />;
+const Container = styled.div`
+  top: 50%;
+  left: 50%;
+  width: 30rem;
+  position: fixed;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 768px) {
+    width: 50rem;
+  }
+`;
+
+const Index: FunctionComponent<PageProps> = () => (
+  <Container>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 342 57">
+      <Defs />
+      <TracedM />
+      <Logo />
+    </svg>
+  </Container>
+);
 
 export default Index;
